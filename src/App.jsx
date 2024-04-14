ebc30fb3218b61e3d6faf1c97403ad85e894b6d4
Mohamed-Ashraf <140234307+MohammedAshrf@users.noreply.github.com>
2024-04-14 06:43:21 +0200

    send files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import OnePage from "./OnePage";
+// import VitePage from "./VitePage";
+
+function App() {
+  return (
+    <div className="app">
+      {/* <VitePage /> */}
+      <OnePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/OnePage.jsx
@@ -0,0 +1,11 @@
+import MainContent from "./components/MainContent";
+import Navbar from "./components/Navbar";
+
+export default function OnePage() {
+  return (
+    <div>
+      <Navbar />
+      <MainContent />
+    </div>
+  );
+}
